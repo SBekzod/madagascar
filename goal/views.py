2c5c26bdc8acf4a7b7a3b51cb62ccdd2bc0d4b50
martin <martin@comets.kr>
2024-07-03 14:19:26 +0900

    feat: templates integration

--- goal/views.py
+++ goal/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
--- plan/urls.py
+++ plan/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from goal.views import (home_view)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home_view),
 ]
